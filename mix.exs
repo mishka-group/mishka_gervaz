0dd8bebee5ff157e0309d7e8b35636e4062afc33
Shahryar Tavakkoli <shahryar.tbiz@gmail.com>
2026-09-15 08:17:01 +0200

    chore: update ci

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule MishkaGervaz.MixProject do
   use Mix.Project
 
-  @version "0.0.1-alpha.5"
+  @version "0.0.1-alpha.6"
   @source_url "https://github.com/mishka-group/mishka_gervaz"
 
   def project do
